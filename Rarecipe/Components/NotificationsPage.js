5f3acba98f0245a3e4eab653e2d6fb08e7c94d91
justicetetteh233 <jaycode233@gmail.com>
2023-07-24 04:05:20 +0000

    setting imports

---
+++ Rarecipe/Components/NotificationsPage.js
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Dimensions } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
